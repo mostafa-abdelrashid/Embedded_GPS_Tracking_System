47c7fd7c544f3768e07a9bddb4f55322dc164c5c
IbrahimAhmed196 <ibrahimahmed1962004@gmail.com>
2025-04-24 00:09:29 +0200

    adding GPIO source and header files and modifing Bit_Utilities header file

---
+++ Headers/MCAL/GPIO.h
@@ -0,0 +1,11 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#define GPIO_LOCK_KEY 0x4C4F434B
+
+extern void initPortA(void);
+extern void initPortB(void);
+extern void initPortD(void);
+extern void initPortF(void);
+
+#endif
