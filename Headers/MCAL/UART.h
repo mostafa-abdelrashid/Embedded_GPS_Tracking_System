6119a4596cb35e2ed2bc4a3fe6a5fba14a5d08d9
Mostafa abdelrashid <mostafa.abdelrashid4@gmail.com>
2025-04-25 05:42:37 +0300

    added UART driver

---
+++ Headers/MCAL/UART.h
@@ -0,0 +1,60 @@
+
+#ifndef UART_H
+#define UART_H
+
+//UART 0(for pc comm),2(for gps comm) communicatiom functions
+extern void UART0_SendChar(char data );
+extern void UART0_SendString(char *str);
+extern char UART0_ReceiveChar(void);
+extern void UART0_ReceiveString(char* cmd, unsigned int length);
+
+extern void UART2_SendChar(char data );
+extern void UART2_SendString(char *str);
+extern char UART2_ReceiveChar(void);
+extern void UART2_ReceiveString(char* cmd, unsigned int length);
+
+//UART5 n\for other peripheral but unused till now
+extern void UART5_SendChar(char data );
+extern void UART5_SendString(char *str);
+extern char UART5_ReceiveChar(void);
+extern void UART5_ReceiveString(char* cmd, unsigned int length);
+
+//UART Initialization functions 
+extern void UART0_Init(void);
+extern void UART2_Init(void);
+extern void UART5_Init(void);
+
+//Identification for all UART modules
+#define UART0   0
+#define UART1   1
+#define UART2   2
+#define UART3   3
+#define UART4   4
+#define UART5   5
+#define UART6   6
+#define UART7   7
+
+//Defining some useful constants
+
+#define UART_PARITY_NONE        0
+#define UART_PARITY_ODD         1
+#define UART_PARITY_EVEN        2
+
+
+#define UART_STOP_BIT_1			1
+#define UART_STOP_BIT_2			2
+
+#define UART_PORTA				0
+#define UART_PORTB				1
+#define UART_PORTC				2
+#define UART_PORTD				3
+#define UART_PORTE				4
+
+#define UART0_PINS    0x03
+#define UART2_PINS		0xC0
+#define UART5_PINS		0x30
+#define CR 0x0D  // "\r"
+#define END 0x2A			// "*" 
+
+
+#endif // UART_H
---
+++ src/MCAL/UART.c
@@ -0,0 +1,142 @@
+
+#include"../../Headers/Mcal/UART.h"
+#include<stdio.h>
+#include"../../Services/tm4c123gh6pm.h"
+#include"../../Services/Bit_Utilities.h"
+
+// UART0_initialization
+void UART0_Init(void){
+    SET(SYSCTL_RCGCUART_R,UART0); // activate UART0 clock
+    while(GET_BIT(SYSCTL_PRUART_R,UART0)==0); // wait for activation
+    SET(SYSCTL_RCGCGPIO_R,UART_PORTA);  // activate PORTA GPIO clock
+    while(GET_BIT(SYSCTL_PRGPIO_R,UART_PORTA)==0);
+    CLR_BIT(UART0_CTL_R,0); // disable UART0
+    UART0_IBRD_R=0x68;      // 104 for 9600 baud @16MHz
+    UART0_FBRD_R=0xB;       // 11
+    UART0_LCRH_R=0x70;      // 8-bits data & enable fifo
+    UART0_CTL_R=0x301;      // enable UART0, RX, TX
+    SET(GPIO_PORTA_AFSEL_R,UART0_PINS); // alt function PA0,PA1
+    GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0xFFFFFF00) + 0x00000011;
+    SET(GPIO_PORTA_DEN_R,UART0_PINS); // digital enable A0,A1
+    CLR(GPIO_PORTA_AMSEL_R,UART0_PINS); // disable analog A0,A1
+}
+
+// UART2_initialization 
+void UART2_Init(void){
+    SET(SYSCTL_RCGCUART_R,UART2); // activate UART2 clock
+    while(GET_BIT(SYSCTL_PRUART_R,UART2)==0);
+    SET(SYSCTL_RCGCGPIO_R,UART_PORTD);  // activate PORTD GPIO clock
+    while(GET_BIT(SYSCTL_PRGPIO_R,UART_PORTD)==0);
+    CLR_BIT(UART2_CTL_R,0); // disable UART2
+    UART2_IBRD_R=0x68;      // Fixed to 16MHz clock (was 80MHz)
+    UART2_FBRD_R=0xB;       // Fixed to match 16MHz
+    UART2_LCRH_R=0x70;      // 8-bits data & enable fifo
+    UART2_CTL_R=0x301;      // enable UART2, RX, TX
+    SET(GPIO_PORTD_AFSEL_R,UART2_PINS); // alt function PD7,PD6
+    GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R & 0x00FFFFFF) + 0x11000000;
+    SET(GPIO_PORTD_DEN_R,UART2_PINS); // digital enable D6,D7
+    CLR(GPIO_PORTD_AMSEL_R,UART2_PINS); // disable analog D6,D7
+}
+
+// UART5_initialization 
+void UART5_Init(void){
+    SET(SYSCTL_RCGCUART_R,UART5); // activate UART5 clock
+    while(GET_BIT(SYSCTL_PRUART_R,UART5)==0);
+    SET(SYSCTL_RCGCGPIO_R,UART_PORTE);  // activate PORTE GPIO clock
+    while(GET_BIT(SYSCTL_PRGPIO_R,UART_PORTE)==0);
+    CLR_BIT(UART5_CTL_R,0); // disable UART5
+    UART5_IBRD_R=0x68;      // 104 for 9600 baud @16MHz
+    UART5_FBRD_R=0xB;       // 11
+    UART5_LCRH_R=0x70;      // 8-bits data & enable fifo
+    UART5_CTL_R=0x301;      // enable UART5, RX, TX
+    SET(GPIO_PORTE_AFSEL_R,UART5_PINS); 
+    GPIO_PORTE_PCTL_R = (GPIO_PORTE_PCTL_R & 0xFF00FFFF) + 0x00110000;
+    SET(GPIO_PORTE_DEN_R,UART5_PINS); 
+    CLR(GPIO_PORTE_AMSEL_R,UART5_PINS); 
+}
+
+// UART0 send char and string functions
+void UART0_SendChar(char data){
+    while(GET_BIT(UART0_FR_R,5)); // Wait while TX FIFO full
+    UART0_DR_R=data;
+}
+
+void UART0_SendString(char* str){
+    while(*str){
+        UART0_SendChar(*str);
+        str++;
+    }
+}
+
+// UART0 receive string and char functions (for debugging through PC terminal)
+char UART0_ReceiveChar(){ 
+    while(GET_BIT(UART0_FR_R,4)); // Wait while RX FIFO empty
+    return (char)UART0_DR_R;
+}
+
+void UART0_ReceiveString(char* ptr, unsigned int length){  
+    char character;
+    for(int i=0;i<length;i++){
+        character = UART0_ReceiveChar();
+        if(character != CR){
+            ptr[i]=character;
+            UART0_SendChar(ptr[i]);
+        }
+        else if(character == CR || i==length){    
+            break;
+        }                    
+    }
+} 
+
+// UART2 receive string and char functions (for communication with GPS)
+char UART2_ReceiveChar(){  
+    while(GET_BIT(UART2_FR_R,4)); // Wait while RX FIFO empty
+    return (char)UART2_DR_R;
+}
+
+void UART2_ReceiveString(char* ptr, unsigned int length){  
+    char character;
+    for(int i=0;i<length;i++){
+        character = UART2_ReceiveChar();
+        if(character != END){
+            ptr[i]=character;
+        }
+        else if(character == END || i==length){    
+            break;
+        }                    
+    }
+}
+
+// UART5 receive string and char functions
+char UART5_ReceiveChar(){  
+    while(GET_BIT(UART5_FR_R,4)); // Wait while RX FIFO empty
+    return (char)UART5_DR_R;
+}
+
+void UART5_ReceiveString(char* ptr, unsigned int length){  //
+    char character;
+    for(int i=0;i<length;i++){
+        character = UART5_ReceiveChar();
+        if(character != END){
+            ptr[i]=character;
+       
+        }
+        else if(character == END || i==length){    
+            break;
+        }                    
+    }
+}
+
+// UART5 send functions 
+void UART5_SendChar(char data){
+    while(GET_BIT(UART5_FR_R,5));
+    UART5_DR_R=data;  
+}
+
+void UART5_SendString(char* ptr){
+    while(*ptr){
+        UART5_SendChar(*ptr);
+        ptr++;
+    }
+}
+
