9aa4fd6c475a213be20471a7074cb5ed3792b672
Akram Mohamed <akram.magdy1000@gmail.com>
2025-04-27 19:00:12 +0300

    Modified Systick.c and added Systick.h

---
+++ Headers/MCAL/Systick.h
@@ -0,0 +1,9 @@
+#ifndef SYSTICK_H
+#define SYSTICK_H
+
+// #include "../../../Services/STD_TYPES.h"
+extern void Systick_Init(void);
+extern void Systick_Wait(unsigned int delay);
+extern void delay (unsigned int delay);
+
+#endif 
