352cbae95f95334c44f70bab3e3ad4cf85316fda
Jimmy-307 <abderhmanahmed43@gmail.com>
2025-05-09 16:02:15 +0300

    LCD (PINS and Commands) Update

--- Headers/HAL/LCD.h
+++ Headers/HAL/LCD.h
@@ -1,20 +1,11 @@
-/*#ifndef LCD_H
-#define LCD_H
-
-extern void initLCD (void);
-extern void printdata(unsigned char data);
-extern void LCD_data(unsigned char data);
-extern void LCD_cmd(unsigned char cmd);
-extern void LCD_string(unsigned char *str,unsigned char len);
-
-#endif
-*/
 #ifndef LCD_H
 #define LCD_H
+#include<stdint.h>
 
-void initLCD(void);
-void LCD_cmd(char cmd);
-void LCD_data(char data);
-void LCD_string(char *str, char len);
+void LCD_Init(void) ;
+void LCD_WriteCommand(uint8_t cmd) ;
+void LCD_WriteData(uint8_t data) ;
+void LCD_String(char* str) ;
+void LCD_Clear(void);
 
 #endif
